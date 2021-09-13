7f36265f619badc58368b65c56700aad0a7f3b99
Leo Kristopher Piel <leo.piel@pipedrive.com>
2021-09-13 19:52:14 +0300

    Improve

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 try {
-  const express = require('express');
+  const express = require("express");
   const app = express();
-  const WebSocket = require('ws');
+  const WebSocket = require("ws");
 
+  app.use(express.static("frontend"));
 
-  app.use(express.static('frontend'));
-
-  app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/frontend/views/index.html');
+  app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/frontend/views/index.html");
   });
 
   // app.post('/submitTeamName', function (req, res) {
   //   try {
   //     console.log('GOT ANSWERS ', req.body);
-
 
   //     res.json({
   //       ...req.body,
@@ -23,49 +21,61 @@ try {
   //   }
   // });
 
-
-
   const socketServer = new WebSocket.Server({ port: 3030 });
   let answerOrder = [];
 
-  socketServer.on('connection', (socketClient) => {
-    socketClient.send(JSON.stringify({ answerOrder }));
+  socketServer.on("connection", (socketClient) => {
+    try {
+      socketClient.send(JSON.stringify({ answerOrder }));
+    } catch (err) {
+      console.log(err);
+    }
 
-    socketClient.on('message', (message) => {
-      const messageString = message.toString();
-      const messageJSON = JSON.parse(messageString);
+    socketClient.on("message", (message) => {
+      try {
+        const messageString = message.toString();
+        const messageJSON = JSON.parse(messageString);
+        console.log(messageJSON);
 
-      if (messageJSON.answerer) {
-        answerOrder.push(messageJSON.answerer);
-      }
-
-      if (messageJSON.reset) {
-        answerOrder = [];
-      }
-
-      socketServer.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          if (messageJSON.answerer || messageJSON.reset) {
-            client.send(JSON.stringify({ answerOrder }));
-          }
-
-          if (messageJSON.reset) {
-            client.send(JSON.stringify({ answerOrder, reset: true }));
-          }
+        if (messageJSON.answerer) {
+          answerOrder.push(messageJSON.answerer);
         }
-      });
+
+        if (messageJSON.reset) {
+          answerOrder = [];
+        }
+
+        socketServer.clients.forEach((client) => {
+          try {
+            if (client.readyState === WebSocket.OPEN) {
+              if (messageJSON.answerer || messageJSON.reset) {
+                client.send(JSON.stringify({ answerOrder }));
+              }
+  
+              if (messageJSON.reset) {
+                client.send(JSON.stringify({ answerOrder, reset: true }));
+              }
+            }
+          } catch (err) {
+            console.log(err);
+          }
+         
+        });
+      } catch (err) {
+        console.log(err);
+      }
     });
 
-    socketClient.on('close', (socketClient) => {
-      console.log('closed');
-      console.log('Number of clients: ', socketServer.clients.size);
+    socketClient.on("close", (socketClient) => {
+      console.log("closed");
+      console.log("Number of clients: ", socketServer.clients.size);
     });
   });
 
-  const port = process.env.PORT || 80;
+  const port = process.env.PORT || 8080;
 
   app.listen(port, function () {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log("Our app is running on http://localhost:" + port);
   });
 } catch (err) {
   console.error(err);
